7872b6577189d39cc5cf024a1ec2a64f342defc5
VNagorniy <vnagorniyrnd@gmail.com>
2025-03-14 16:37:44 +0300

    feat: add pagination for characters on CharacherPage

--- src/pages/CharacterPage/CharacterPage.jsx
+++ src/pages/CharacterPage/CharacterPage.jsx
@@ -5,26 +5,60 @@ import s from './CharacterPage.module.css';
 export const CharacterPage = () => {
 	const [characters, setCharacters] = useState([]);
 
+	const [info, setInfo] = useState({
+		count: 0,
+		pages: 0,
+		prev: null,
+		next: null
+	});
+
 	useEffect(() => {
 		axios.get('https://rickandmortyapi.com/api/character').then((res) => {
 			setCharacters(res.data.results);
+			setInfo(res.data.info);
 		});
 	}, []);
+
+	const nextPageHandler = () => {
+		axios.get(info.next).then((res) => {
+			setCharacters(res.data.results);
+			setInfo(res.data.info);
+		});
+	};
+
+	const previousPageHandler = () => {
+		axios.get(info.prev).then((res) => {
+			setCharacters(res.data.results);
+			setInfo(res.data.info);
+		});
+	};
 
 	return (
 		<div className={'pageContainer'}>
 			<h1 className={'pageTitle'}>CharacterPage</h1>
 			{characters.length && (
-				<div className={s.characters}>
-					{characters.map((character) => {
-						return (
-							<div key={character.id} className={s.character}>
-								<div className={s.characterLink}>{character.name}</div>
-								<img src={character.image} alt={`${character.name} avatar`} />
-							</div>
-						);
-					})}
-				</div>
+				<>
+					{
+						<div className={s.characters}>
+							{characters.map((character) => {
+								return (
+									<div key={character.id} className={s.character}>
+										<div className={s.characterLink}>{character.name}</div>
+										<img src={character.image} alt={`${character.name} avatar`} />
+									</div>
+								);
+							})}
+						</div>
+					}
+					<div className={s.buttonContainer}>
+						<button className="linkButton" onClick={previousPageHandler}>
+							Назад
+						</button>
+						<button className="linkButton" onClick={nextPageHandler}>
+							Вперед
+						</button>
+					</div>
+				</>
 			)}
 		</div>
 	);
