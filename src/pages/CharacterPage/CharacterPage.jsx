2b52abfa3609381df3d53c7aae631947c9f2669c
VNagorniy <vnagorniyrnd@gmail.com>
2025-03-14 16:22:35 +0300

    feat: add axios fetch for rendering CharacherPage

--- src/pages/CharacterPage/CharacterPage.jsx
+++ src/pages/CharacterPage/CharacterPage.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import s from './CharacterPage.module.css';
+
 export const CharacterPage = () => {
+	const [characters, setCharacters] = useState([]);
+
+	useEffect(() => {
+		axios.get('https://rickandmortyapi.com/api/character').then((res) => {
+			setCharacters(res.data.results);
+		});
+	}, []);
+
 	return (
-		<div>
+		<div className={'pageContainer'}>
 			<h1 className={'pageTitle'}>CharacterPage</h1>
+			{characters.length && (
+				<div className={s.characters}>
+					{characters.map((character) => {
+						return (
+							<div key={character.id} className={s.character}>
+								<div className={s.characterLink}>{character.name}</div>
+								<img src={character.image} alt={`${character.name} avatar`} />
+							</div>
+						);
+					})}
+				</div>
+			)}
 		</div>
 	);
 };
