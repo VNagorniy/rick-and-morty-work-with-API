7f8213594cc141603e36b8804f481a12a0963f85
VNagorniy <vnagorniyrnd@gmail.com>
2025-03-22 15:21:23 +0300

    feat: add axios fetch for rendering LocationPage и EpisodePage

--- src/pages/EpisodePage/EpisodePage.jsx
+++ src/pages/EpisodePage/EpisodePage.jsx
@@ -1,7 +1,41 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export const EpisodePage = () => {
+	const [episodes, setEpisodes] = useState([]);
+
+	useEffect(() => {
+		axios.get('https://rickandmortyapi.com/api/episode').then((res) => {
+			setEpisodes(res.data.results);
+		});
+	}, []);
+
 	return (
-		<div>
+		<div className={'pageContainer'}>
 			<h1 className={`pageTitle`}>EpisodePage</h1>
+			<div>
+				{episodes.map((episode) => {
+					return (
+						<div key={episode.id}>
+							<ul>
+								<li>
+									Эпизод: <b>{episode.episode}</b>
+								</li>
+								<li>
+									Название эпизода: <b>{episode.name}</b>
+								</li>
+								<li>
+									Дата выхода эпизода в эфир: <b>{episode.air_date}</b>
+								</li>
+								<li>
+									Список персонажей, которые были замечены в эпизоде: <b>{episode.characters.length}</b>
+								</li>
+							</ul>
+							<hr />
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
--- src/pages/LocationPage/LocationPage.jsx
+++ src/pages/LocationPage/LocationPage.jsx
@@ -1,7 +1,42 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export const LocationPage = () => {
+	const [locations, setLocations] = useState([]);
+
+	useEffect(() => {
+		axios.get('https://rickandmortyapi.com/api/location').then((res) => {
+			console.log(res);
+			setLocations(res.data.results);
+		});
+	}, []);
+
 	return (
-		<div>
+		<div className={`pageContainer`}>
 			<h1 className={`pageTitle`}>LocationPage</h1>
+			<div>
+				{locations.map((location) => {
+					return (
+						<div key={location.id}>
+							<ul>
+								<li>
+									Название локации: <b>{location.name}</b>
+								</li>
+								<li>
+									Тип локации: <b>{location.type}</b>
+								</li>
+								<li>
+									Измерение, в котором находится местоположение: <b>{location.dimension}</b>
+								</li>
+								<li>
+									Количество персонажей, которых видели в данной локации: <b>{location.residents.length}</b>
+								</li>
+							</ul>
+							<hr></hr>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
